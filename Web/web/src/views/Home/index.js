4aabb3aacc0f9efd00e380b6d79123dcbde70731
Marcos Vinicius <mviniciusrj21@gmail.com>
2021-08-08 23:47:38 -0300

    Cabeçalho Finalizado

---
+++ Web/web/src/views/Home/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as S from './styles'
+
+///Nossos Components
+import Header from '../../Components/Header/Index';
+
+function Home() {
+  return  (
+  
+    <S.Container>
+    
+    <Header/>
+    
+    </S.Container>
+  
+  )
+}
+
+export default Home;
